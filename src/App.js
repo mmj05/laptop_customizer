afc49192519edc6670328eea209e204bfe69069b
Muhaimin Jobayer <mahmudmuhaimin@yahoo.com>
2020-08-25 12:26:20 -0500

    recreated components so the required styling mathes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customize from './Customize/Customize';
+import Cart from './Cart/Cart';
+import FEATURES from './FEATURES/FEATURES'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props) 
+      this.state = {
+        selected: {
+          Processor: {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+          'Operating System': {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+          'Video Card': {
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+          Display: {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          }
+        }
+      };
+  
+  }
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  };
+
+  render() {
+
+    return (
+      <div className="App">
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
+        <main>
+          <Customize 
+            selected={this.state.selected}
+            features={FEATURES}
+            updateFeature={this.updateFeature}
+          />
+          <Cart 
+            selected={this.state.selected}
+          />
+          
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
